5089fece5535996cd5fc847298bb6041139cc124
discdiver <jeffmshale@gmail.com>
2018-12-19 14:18:54 -0500

    updates app to accept 3gp, flv, and mkv video file types

--- frames.py
+++ frames.py
@@ -7,9 +7,7 @@ import numpy as np
 
 top = os.getcwd()
 
-print('hello')
-
-def make_images(video):
+def make_images(video, max_images = 1000):
     '''
     Output an image for each of first 1000 frames in each video
 
@@ -20,28 +18,28 @@ def make_images(video):
     '''
 
     file_name = os.path.split(video)[1]
-    file_no_ext = file_name.split(".")[0]   # watch out for . in file name
+    file_no_ext = file_name.split(".")[0]
 
     vidcap = cv2.VideoCapture(video)
     vid_length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    count = 0
+    count = 0             # counter to stay under max images
     #print("/pfs/out/{}".format(file_no_ext))
     # os.mkdir("/pfs/out/{}".format(file_no_ext))  #cv2 requires directory to exist to write to it
 
-    while count < 1000 and count < vid_length:
+    while count < max_images and count < vid_length:
         try:
             success, image = vidcap.read()
-            cv2.imwrite(os.path.join("/pfs/out", file_no_ext+"frame{:d}.jpg".format(count)), image)
-            # TESTING LOCALLY
-            # cv2.imwrite(video+"frame{:d}.jpg".format(count), image)
+            cv2.imwrite(os.path.join("/pfs/out", file_no_ext + "frame{:d}.jpg".format(count)), image)
         except Exception as e:
             print("Oops, there was an exception: {}".format(e))
 
         count += 1
 
+ok_file_type = {'mp4', '3gp', 'flv', 'mkv'}
+
 # walk /pfs/videos and call make_images on every file found
 for dirpath, dirs, files in os.walk("/pfs/videos"):
     for file in files:
-        if file[-3:] == 'mp4':  # change to in vidtypes and define set vidtypes
+        if file[-3:] in ok_file_type:
             make_images(os.path.join(dirpath, file))
